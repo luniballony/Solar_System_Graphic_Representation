f42c29be0675199321bfbf1031f8df7247af281b
Matilde <misabelpcarmo@gmail.com>
2024-11-28 17:09:19 +0000

    Created Constants.Js
    Seperated constants by creating a different file and calling them. In html page Sun and Earth are shown with different sizes and colors

---
+++ constants.js
@@ -0,0 +1,57 @@
+// constants.js
+
+// Speeds
+/* export const Positions = {
+    Sun : 1,
+    Mercury: (Sun + 1),
+    Venus: (Mercury + 1),
+    Earth: (Venus + 1),
+    Mars: (Earth + 1),
+    Jupiter: (Mars + 1),
+    Saturn: (Jupiter + 1),
+    Uranus: (Saturn + 1),
+    Neptune: (Uranus + 1),
+}; */
+
+// Sizes 
+/* export const Sizes = {
+    Sun : 8,
+    Mercury: (Sun * 1),
+    Venus: (Sun * 1),
+    Earth: (Sun * 1),
+    Mars: (Sun * 1),
+    Jupiter: (Sun * 1),
+    Saturn: (Sun * 1),
+    Uranus: (Sun * 1),
+    Neptune: (Sun * 1),
+}; */
+
+export const Sizes = {
+    Sun : 8,
+    Mercury: 2,
+    Venus: 2,
+    Earth: 2,
+    Mars: 2,
+    Jupiter: 2,
+    Saturn: 2,
+    Uranus: 2,
+    Neptune: 2,
+};
+ 
+// Colors
+export const Colors = {
+    Sun : 0xE3AD27,
+    Mercury : 0xCC0000,
+    Venus : 0xCC0000,
+    Earth : 0x4FB06D,
+    Mars : 0xCC0000,
+    Jupiter : 0xCC0000,
+    Saturn : 0xCC0000,
+    Uranos : 0xCC0000,
+    Neptune : 0xCC0000, 
+    Stars : 0xffffff,
+};
+
+// speed of self rotation
+
+
